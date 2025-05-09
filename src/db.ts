1087726f28caf55308c119a343df778a7a2929aa
unknown <mohdarish8482@gmail.com>
2025-05-09 20:57:01 +0530

    added routes and their bussiness logic again from scratch

---
+++ src/db.ts
@@ -0,0 +1,33 @@
+import mongoose, { model, Schema } from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://mohdarish8482:babbanbillo8482@learning-cluster1.91lka6y.mongodb.net/second-brain"
+);
+
+const UserSchema = new Schema({
+  username: { type: String, unique: true },
+  password: String,
+});
+
+export const UserModel = model("User", UserSchema);
+
+const ContentSchema = new Schema({
+  title: String,
+  link: String,
+  tags: [{ type: mongoose.Types.ObjectId, ref: "Tag" }],
+  type: String,
+  userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+});
+
+const LinkSchema = new Schema({
+  hash: String,
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
+});
+
+export const LinkModel = model("Links", LinkSchema);
+export const ContentModel = model("Content", ContentSchema);
